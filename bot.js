e95374eae6c852c16b1952913f853bd475325786
jnti <johnim2247@gmail.com>
2019-09-19 22:29:54 -0400

    Cleaning up code base

--- bot.js
+++
@@ -1,7 +0,0 @@
-var itemName;
-
-// function to get the item name onClick of button
-function getItemName(){
-    var itemName = document.getElementById("itemName").value;
-    document.write(itemName);
-}
