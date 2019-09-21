f92117e72b49d9870834f5d8282febbd8f7738d7
jnti <johnim2247@gmail.com>
2019-09-20 23:23:54 -0400

    Starting Post Request
    Initializing and creating post request

--- postrequest.js
+++ postrequest.js
@@ -1,1 +1,15 @@
-// add the item to cart
+var http = new XMLHttpRequest();
+var url = "https://www.supremenewyork.com/shop/" + itemName.id + "/add.json";
+var params = "";
+
+http.open('POST', url, true);
+
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+        alert(http.responseText);
+    }
+}
+
+http.send(params);
