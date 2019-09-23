7b28ef10c242ab0bc216c1aba1fb0d0b37a4816d
jnti <johnim2247@gmail.com>
2019-09-23 15:04:48 -0400

    Added the ability to get the size id
    Used another for loop to loop through and get the sizes id from the style json

--- jsonParser.js
+++ jsonParser.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var mobileEndpoint = "https://www.supremenewyork.com/mobile_stock.json";
 var ssEndpoint;
-var itemName, itemTextValue, itemStyle, itemColorValue;
+var itemName, itemTextValue, itemStyle, itemColorValue, itemSize;
 
 // onClick of the submit button
 // doing a simplified http request to parse the products_and_categories
@@ -18,6 +18,7 @@ document.getElementById("submit").addEventListener("click", function () {
             // get the text value of the textbox
             itemTextValue = document.getElementById("itemTextBox").value;
             itemColorValue = document.getElementById("itemColorTextBox").value;
+            itemSizeValue = document.getElementById("itemSizeTextBox").value;
 
             // loop through array of json and if the name is equal to the name of text value
             // write the id of the respective item to the page
@@ -42,12 +43,23 @@ document.getElementById("submit").addEventListener("click", function () {
 
                             // outputting the results of the styles request into space3 html element
                             var dataAfterStringify = JSON.stringify(ssData);
-                            document.getElementById("space3").innerHTML = dataAfterStringify;
+                            document.getElementById("space4").innerHTML = dataAfterStringify;
 
                             // loop through array of json and if the name is equal to the name of text value
                             // write the id of the respective item to the page
                             for (var i = 0; i < ssData.length; i++) {
                                 itemStyle = ssData[i];
+                                console.log(itemStyle.sizes);
+
+                                // loop through array of json and if the name is equal to the name of text value
+                                // write the id of the respective item to the page
+                                for (var j = 0; j < ssData[i].sizes.length; j++) {
+                                    itemSize = ssData[i].sizes[j];
+                                    
+                                    if (itemSize.name == itemSizeValue) {
+                                        document.getElementById("space3").innerHTML = itemSize.id;
+                                    }
+                                }
 
                                 if (itemStyle.name == itemColorValue) {
                                     document.getElementById("space2").innerHTML = itemStyle.id;
@@ -61,5 +73,3 @@ document.getElementById("submit").addEventListener("click", function () {
         }
     })
 });
-
-// get the style and size id from https://www.supremenewyork.com/shop/<id>.json
