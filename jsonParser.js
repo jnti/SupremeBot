82bd09be1794a3b8138070534746e7c59fa1f1bb
jnti <johnim2247@gmail.com>
2019-09-26 19:24:28 -0400

    Added PostRequest for add.json
    Able to do a post request and getting the responseText correctly

--- jsonParser.js
+++ jsonParser.js
@@ -1,11 +1,11 @@
 var request = require('request');
 var mobileEndpoint = "https://www.supremenewyork.com/mobile_stock.json";
 var ssEndpoint;
-var itemName, itemTextValue, itemStyle, itemColorValue, itemSize;
+var itemName, itemTextValue, itemStyle, itemColorValue, itemSize, itemId, itemStyleId, itemSizeId;
 
 // onClick of the submit button
 // doing a simplified http request to parse the products_and_categories
-document.getElementById("submit").addEventListener("click", function () {
+document.getElementById("loadId").addEventListener("click", function () {
     request({
         url: mobileEndpoint,
         json: true
@@ -24,9 +24,10 @@ document.getElementById("submit").addEventListener("click", function () {
             // write the id of the respective item to the page
             for (var i = 0; i < data.length; i++) {
                 itemName = data[i];
-                
+
                 if (itemName.name == itemTextValue) {
                     document.getElementById("space1").innerHTML = itemName.id;
+                    itemId = itemName.id;
 
                     // style and size endpoint of the item
                     ssEndpoint = "https://www.supremenewyork.com/shop/" + itemName.id + ".json";
@@ -49,25 +50,27 @@ document.getElementById("submit").addEventListener("click", function () {
                             // write the id of the respective item to the page
                             for (var i = 0; i < ssData.length; i++) {
                                 itemStyle = ssData[i];
-                                console.log(itemStyle.sizes);
 
                                 // loop through array of json and if the name is equal to the name of text value
                                 // write the id of the respective item to the page
                                 for (var j = 0; j < ssData[i].sizes.length; j++) {
                                     itemSize = ssData[i].sizes[j];
-                                    
+
+                                    // getting the item size id
                                     if (itemSize.name == itemSizeValue) {
                                         document.getElementById("space3").innerHTML = itemSize.id;
+                                        itemSizeId = itemSize.id;
                                     }
                                 }
 
+                                // getting the item style id
                                 if (itemStyle.name == itemColorValue) {
                                     document.getElementById("space2").innerHTML = itemStyle.id;
+                                    itemStyleId = itemStyle.id;
                                 }
                             }
                         }
                     })
-
                 }
             }
         }
